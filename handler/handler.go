fe806163442b5e614ab16e0f2c84cae5f1469721
Andrew_Qi <qf353208262@gmail.com>
2020-01-15 10:08:54 +0800

    feature: Complete File Upload Functions

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 // handle upload file
@@ -15,8 +17,31 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 			io.WriteString(w, "internel server error")
 			return
 		}
-		io.WriteString(w,string(data))
+		io.WriteString(w, string(data))
 	} else if r.Method == "POST" {
 		//restore stream-file to local storage
+		file, head, err := r.FormFile("file")
+		if err != nil {
+			fmt.Println("Failed to get data, err:", err.Error())
+			return
+		}
+		defer file.Close()
+		newFile, err := os.Create("../../../Storage/" + head.Filename)
+		if err != nil {
+			fmt.Println("Failed to create file, err:", err.Error())
+			return
+		}
+		defer newFile.Close()
+		_,err = io.Copy(newFile, file)
+		if  err!= nil{
+			fmt.Println("Failed to save data into file, err:",err.Error())
+			return
+		}
+		http.Redirect(w,r,"/file/upload/suc", http.StatusFound)
 	}
+}
+
+
+func UploadSucHandler(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w,"Upload finished!")
 }
