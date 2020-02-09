0416647c59f6e5eb2076a98394810566b686cfe7
Fan Qi <353208262@qq.com>
2020-02-09 15:00:04 +0800

    feature: add fastupload interface

--- main.go
+++ main.go
@@ -21,6 +21,10 @@ func main() {
 	http.HandleFunc("/user/signup", handler.SignUpHandler)
 	http.HandleFunc("/user/signin", handler.SignInHandler)
 	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
+
+	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(
+		handler.TryFastUploadHandler))
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("Fail to start server, err:%s", err.Error())
