340986d98ce70f456f1dd765b3f608f133166954
Fan Qi <353208262@qq.com>
2020-02-10 16:05:01 +0800

    feature: add test mpupload

--- main.go
+++ main.go
@@ -18,12 +18,21 @@ func main() {
 	http.HandleFunc("/file/download", handler.DownloadHandler)
 	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
 	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
+
 	http.HandleFunc("/user/signup", handler.SignUpHandler)
 	http.HandleFunc("/user/signin", handler.SignInHandler)
 	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
 
 	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(
 		handler.TryFastUploadHandler))
+
+	// 分块上传接口
+	http.HandleFunc("/file/mpupload/init",
+		handler.HTTPInterceptor(handler.InitialMultipartUploadHandler))
+	http.HandleFunc("/file/mpupload/uppart",
+		handler.HTTPInterceptor(handler.UploadPartHandler))
+	http.HandleFunc("/file/mpupload/complete",
+		handler.HTTPInterceptor(handler.CompleteUploadHandler))
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
